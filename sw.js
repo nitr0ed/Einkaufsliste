b5fc6468d431f3dec37297d548ccce2b099bf12c
nitr0_ed <edmund.voelkl@gmx.de>
2026-04-17 21:07:35 +0200

    Initial

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "pwa-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html"
+];
+
+// Install
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch (Cache First)
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
